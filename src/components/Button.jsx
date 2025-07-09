9529b2d3c8ebca4630d39a75af5a580571dfa465
RealMinTech <adesinamariam7@gmail.com>
2025-07-09 16:15:27 +0100

    js

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,23 @@
-const Button = () => {
+const Button = (props) => {
+  const handleSubmit = () => {
+    alert("You have successfully added this product to cart.")
+  }
   return (
     <>
-     <button style={{backgroundColor: "navy", color: "white", padding: "8px", border: "none", borderRadius: "6px"}}>Add to cart</button>
+      <button
+        style={{
+          backgroundColor: "navy",
+          color: "white",
+          padding: "8px",
+          border: "none",
+          borderRadius: "6px",
+        }}
+        onClick={handleSubmit}
+      >
+        {props.title}
+      </button>
     </>
-  )
+  );
 }
 
 export default Button
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 // import profileImage from "/manOnLaptop.webp"
 import Button from './Button'
+import { FaSearch } from 'react-icons/fa'
+// import { FaTimes } from 'react-icons/fa'
+// import { FaCartArrowDown } from 'react-icons/fa6'
 const Card = () => {
+  const [count, setCount] = useState(0)
+  const [modal, setModal] = useState(false)
+  const handleCount = () => {
+     setCount(count => count + 1)
+  }  
+
+  const handleModal =() => {
+    setModal(true)
+  }
+  const data = [
+    {
+      name: "Laptop",
+      img: "/manOnLaptop.webp",
+      price: "#450",
+      desc: "lljfdjdshhjfdhhdfhjkdfvjhdfhfd",
+    },
+    {
+      name: "Laptop",
+      img: "/manOnLaptop.webp",
+      price: "#450",
+      desc: "lljfdjdshhjfdhhdfhjkdfvjhdfhfd",
+    },
+    {
+      name: "Laptop",
+      img: "/manOnLaptop.webp",
+      price: "#450",
+      desc: "lljfdjdshhjfdhhdfhjkdfvjhdfhfd",
+      icon: <FaSearch />
+    },
+  ];
   return (
     <>
-     <div>
+      {data.length >= 3 ? data.map((item, index) => (
+        <div key={index}>
+          <img src={item.img} alt="" />
+          <p>{item.name}</p>
+          <p>{item.desc}</p>
+          <p>{item.price}</p>
+          {/* <FaTimes /> */}
+          {/* <FaCartArrowDown /> */}
+          <span>{item.icon}</span>
+          <Button title="Add to cart" />
+          <Button title="Buy now" />
+        </div>
+      )): "No available data"}
+      <div>{modal}</div>
+      <button onClick={handleModal}>Click</button>
+{
+  modal && <h1>THis is modal component</h1>
+}
+      {/* <div>
         <div>
-            <img src="/manOnLaptop.webp" alt="" className='profile ' />
+          <img src="/manOnLaptop.webp" alt="" className="profile " />
         </div>
         <div>
-            <h3 className='text-3xl text-green-500'>Laptop</h3>
-            <p>Lorem ipsum dolor  voluptatibus vero provident illo quos vel velit beatae. Quas totam consectetur velit aspernatur.</p>
-            <p>#350</p>
-            <Button />
+          <h3 className="text-3xl text-green-500">Laptop</h3>
+          <p>
+            Lorem ipsum dolor voluptatibus vero provident illo quos vel velit
+            beatae. Quas totam consectetur velit aspernatur.
+          </p>
+          <p>#350</p>
+          <Button />
+          <p>{count}</p>
+          <button onClick={handleCount}>Count</button>
         </div>
-     </div>
+      </div> */}
     </>
-  )
+  );
 }
 
 export default Card
